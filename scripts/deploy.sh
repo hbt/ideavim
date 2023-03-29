c10df7c762f4b7b104b648dc669f2a1ab2845738
hbt <hassenbentanfous@gmail.com>
2023-03-29 19:28:35 -0400

    add deploy script for all IDEs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
 # Usage: ./scripts/deploy.sh
+# Deploys the plugin to all the IDEs
 
 set -e
 
+# get the script's path
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd "$SCRIPT_PATH"/.. || exit 1
+
 ./gradlew buildPlugin
 
+for dir in /home/hassen/.local/share/JetBrains/*2022*; do
+  if [[ -d "$dir" ]]; then
+    echo "Installing IdeaVim in $dir"
+    cd "$SCRIPT_PATH"/.. || exit 1
+    cp build/distributions/IdeaVim-SNAPSHOT.zip "$dir"/ideavim.zip
+    cd "$dir" || exit 1
+    rm -rf "$dir"/IdeaVim &> /dev/null
+    unzip ideavim.zip
+  fi
+done
 
-cp build/distributions/IdeaVim-SNAPSHOT.zip /home/hassen/.local/share/JetBrains/IntelliJIdea2022.3/ideavim.zip
-cd /home/hassen/.local/share/JetBrains/IntelliJIdea2022.3 || exit 1
-rm -rf IdeaVim &> /dev/null
-
-unzip ideavim.zip
-
-
-# // TODO(hbt) NEXT apply to all IDEs
 # // TODO(hbt) NEXT add the build zip to the release in github
